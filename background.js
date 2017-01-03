d567cc5b693f953656e4070771d333e327dee0f7
Tomokazu Kizawa <Tomokazu Kizawa>
2017-01-03 23:54:19 +0900

    Modify Test

--- background.js
+++ background.js
@@ -1,13 +1,11 @@
 // コンテキストメニューに "Search Google for "%s"" を追加する
-browser.contextMenus.create({
-    id: 'searchGoogleForSelection',
-    title: 'Googleで検索(ごめんねBing) "%s"', // %s は選択している文字列で置き換わる
-    contexts: ['selection'], // 選択しているときのみメニューに表示される
-    onclick: (info, tab) => {
-        // クリックのイベント
-        // 新しいタブを開いてGoogleで検索する
-        browser.tabs.create({
-            url: 'https://www.google.com/search?q=' + info.selectionText
-        });
-    }
+browser.contextMenus.create({    
+    id:   'searchGoogleForSelection',
+        title:   'Googleで検索(ごめんねBing) "%s"',
+      // %s は選択している文字列で置き換わる
+        contexts:  ['selection'],
+      // 選択しているときのみメニューに表示される
+        onclick:  (info,  tab)  =>  {         // クリックのイベント
+                 // 新しいタブを開いてGoogleで検索する
+                 browser.tabs.create({             url:   'https://www.google.com/search?q='  +  info.selectionText         });     }
 });
